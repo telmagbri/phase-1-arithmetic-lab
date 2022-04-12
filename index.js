44564f3da9d81b9c50252c3fa8fed66f5fd0b51e
Taha <taha.elmagbri2014@gmail.com>
2022-04-12 01:18:39 -0400

    commit tree c56312c8042a55da775f9919d1b158e0b4c489f4

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 20;
+const num4 = 16;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,20);
